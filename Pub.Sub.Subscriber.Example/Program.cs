3b0792a44419aa86b3b7c6e29170546730360a9a
selcancamsari <61784573+selcancamsari@users.noreply.github.com>
2024-02-29 17:02:14 +0300

    redis pub/sub

---
+++ Pub.Sub.Subscriber.Example/Program.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+ISubscriber subscriber = connection.GetSubscriber();
+
+//her türlü mesajı gösterir
+//await subscriber.SubscribeAsync("mychannel", (channel, message) =>
+//{
+//    Console.WriteLine(message);
+//});
+
+//belirli bir patternde olan mesajlerı gösterir
+await subscriber.SubscribeAsync("mychannel.*", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+Console.Read();
