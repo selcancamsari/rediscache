5b5f78db277584b9dbf695a3776dc6359660beca
selcancamsari <61784573+selcancamsari@users.noreply.github.com>
2024-03-01 16:02:24 +0300

    sentinel

---
+++ Sentinel/Controllers/RedisController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sentinel.Services;
+
+namespace Sentinel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        [HttpGet("[action]/{key}/{value}")]
+        public async Task<IActionResult> SetValue(string key, string value)
+        {
+            var redis = await RedisService.RedisMasterDatabase();
+            await redis.StringSetAsync(key, value);
+            return Ok();
+        }
+
+        [HttpGet("[action]/{key}/{value}")]
+        public async Task<IActionResult> GetValue(string key)
+        {
+            var redis = await RedisService.RedisMasterDatabase();
+            var data = await redis.StringGetAsync(key);
+            //bu şekilde yazarsak isInteger, isNull, isNullorEmpty, hasValue bilgileri dönüyor
+            //return Ok(data);
+
+            //bu şekilde yazarsak key'e ait olan value bilgisini dönüyor
+            return Ok(data.ToString());
+        }
+    }
+}
